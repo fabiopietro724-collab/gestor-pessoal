c7dfa50d8994c6675f809db829bd8c30ed31bac6
fabiopietro724-collab <fabiopietro724@gmail.com>
2026-08-02 00:14:43 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
