ff8052bb5f72d503e03215285aa1e9a3b6831af3
fabiopietro724-collab <fabiopietro724@gmail.com>
2026-08-02 00:23:21 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -14,13 +15,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Em breve vou ajudar você a controlar receitas, despesas e metas."
     )
 
-def main():
+async def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     print("Bot iniciado...")
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
